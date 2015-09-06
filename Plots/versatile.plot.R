218b46a3ca6d5f92dacb710ae1601d54a132932e
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-09-06 15:01:30 -0700

    Update versatile.plot.R
    Text added, "Make sure to include a specific value for pch, or the code doesn't work." This was verified by experience in R.

--- Plots/versatile.plot.R
+++ Plots/versatile.plot.R
@@ -2,17 +2,17 @@
 #The structure of this code is to plot points in a matrix according to a chosen (linear) threshhold
 #The points are coloured according to the data input 
 #The viewing window can be adjusted (data.window)
+#Make sure to include a specific value for pch, or the code doesn't work.
 #
-#
-versatile.plot<-function(data,data.window,thresh,ret,cex,xlim=NULL,ylim=NULL,xlab="",ylab="",rampPalette,col,pch=""){
-  ifelse(col=="ramp",
+versatile.plot<-function(data,data.window,thresh,ret,cex,xlim=NULL,ylim=NULL,xlab="",ylab="",colour,pch=pch){
+  ifelse(colour=="ramp",
     plot(data[which(data.window>thresh,arr.in=T)[,1]]~ret[which(data.window>thresh,arr.in=T)[,2]],
     cex=cex,xlab=xlab,ylab=ylab,pch=pch,
     col=rampPalette(length(data[which(data.window>thresh,arr.in=T)]))[rank(data[which(data.window>thresh,arr.in=T)])],
     xlim=xlim, ylim=ylim),
     plot(data[which(data.window>thresh,arr.in=T)[,1]]~ret[which(data.window>thresh,arr.in=T)[,2]],
     cex=cex,xlab=xlab,ylab=ylab,
-    col=col,pch=pch,
+    col=colour,pch=pch,
     xlim=xlim,
     ylim=ylim)
     )
