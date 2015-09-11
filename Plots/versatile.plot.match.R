ca962bb17e6a5ecd921cfadd5fe8f1c0b3330ca4
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-09-11 00:34:18 -0700

    Gave pch option a default value

--- Plots/versatile.plot.match.R
+++ Plots/versatile.plot.match.R
@@ -1,10 +1,10 @@
 #Special plotting
 #Do not need to add "m/z" in the column index for mzvec
 #mzvec is the 9-column matrix result from nagenmz()
-versatile.plot.match<-function(data,mzvec,thresh,cex,col,ylim=NULL,xlim=NULL,xlab="",ylab="",pch){
+versatile.plot.match<-function(data,mzvec,thresh,cex,col,ylim=NULL,xlim=NULL,xlab="",ylab="",pch=1){
   plot(data[round(data[,2],0) %in% mzvec,][which(data[round(data[,2],0) %in% mzvec,]>thresh,arr.in=T)[,1],2]~data[1,][which(data[round(data[,2],0) %in% mzvec,]>thresh,arr.in=T)[,2]],cex=cex,col=col,xlim=xlim,ylim=ylim,xlab=xlab,ylab=ylab,pch=pch)
 }
 #Special plotting
-versatile.points.match<-function(data,mzvec,thresh,cex,col,ylim=NULL,xlim=NULL,xlab="",ylab="",pch){
+versatile.points.match<-function(data,mzvec,thresh,cex,col,ylim=NULL,xlim=NULL,xlab="",ylab="",pch=1){
   points(data[round(data[,2],0) %in% mzvec,][which(data[round(data[,2],0) %in% mzvec,]>thresh,arr.in=T)[,1],2]~data[1,][which(data[round(data[,2],0) %in% mzvec,]>thresh,arr.in=T)[,2]],cex=cex,col=col,xlim=xlim,ylim=ylim,xlab=xlab,ylab=ylab,pch=pch)
 }
