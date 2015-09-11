ea1e8ebb037ba6a3efe4df668a751f963e53dc94
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-09-11 12:16:45 -0700

    Added the option to stretch and/or move mig time
    Now you can move and/or stretch both in the m/z direction and the migration time (retention time) direction.

--- Plots/versatile.plot.move.R
+++ Plots/versatile.plot.move.R
@@ -1,4 +1,6 @@
 #for translating cutoff mass spectrometry data in the m/z direction and stretching in the m/z direction
-versatile.points.move<-function(data,move,stretch,mzvec,thresh,cex,col,ylim=NULL,xlim=NULL,xlab="",ylab="",pch=""){
-  points(stretch*(data[round(data[,2],0) %in% mzvec,][which(data[round(data[,2],0) %in% mzvec,]>thresh,arr.in=T)[,1],2]+move)~data[1,][which(data[round(data[,2],0) %in% mzvec,]>thresh,arr.in=T)[,2]],cex=cex,col=col,xlim=xlim,ylim=ylim,xlab=xlab,ylab=ylab,pch=pch)
+versatile.points.move<-function(data,movemz,stretchmz,movemt,stretchmt,mzvec,thresh,cex,col,ylim=NULL,xlim=NULL,xlab="",ylab="",pch=""){
+  points(stretchmz*(data[round(data[,2],0) %in% mzvec,][which(data[round(data[,2],0) %in% mzvec,]>thresh,arr.in=T)[,1],2]+movemz)~
+  stretchmt*(data[1,][which(data[round(data[,2],0) %in% mzvec,]>thresh,arr.in=T)[,2]]+movemt),
+  cex=cex,col=col,xlim=xlim,ylim=ylim,xlab=xlab,ylab=ylab,pch=pch)
 }
