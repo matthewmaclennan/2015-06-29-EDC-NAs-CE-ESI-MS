28d3135e8099a8120d59091b4fc9a55d03656e82
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-09-14 14:41:28 -0700

    Revert to previous code

--- Plots/versatile.plot.move.R
+++ Plots/versatile.plot.move.R
@@ -1,6 +1,6 @@
 #for translating cutoff mass spectrometry data in the m/z direction and stretching in the m/z direction
 versatile.points.move<-function(data,movemz=0,stretchmz=1,movemt=0,stretchmt=1,mzvec,thresh,cex,col,ylim=NULL,xlim=NULL,xlab="",ylab="",pch=1){
   points(stretchmz*(data[round(data[,2],0) %in% mzvec,][which(data[round(data[,2],0) %in% mzvec,]>thresh,arr.in=T)[,1],2]+movemz)~
-  (stretchmt*(data[1,][which(data[round(data[,2],0) %in% mzvec,]>thresh,arr.in=T)[,2]]+movemt)),
+  (data[1,][which(data[round(data[,2],0) %in% mzvec,]>thresh,arr.in=T)[,2]]),
   cex=cex,col=col,xlim=xlim,ylim=ylim,xlab=xlab,ylab=ylab,pch=pch)
 }
